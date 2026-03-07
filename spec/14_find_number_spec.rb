25d89b438b497410697521b5394fcd030108c6e1
Ivan Yurchak <ivanyurchak3@gmail.com>
2026-03-07 19:09:48 +0100

    Partially solve 14_find_number

--- spec/14_find_number_spec.rb
+++ spec/14_find_number_spec.rb
@@ -114,7 +114,7 @@ describe FindNumber do
     # Create a random_number double called 'number_guessing'. Allow the double
     # to receive 'value' and return the value of 8, in one of the two ways
     # explained above.
-
+    let(:number_guessing) { double('number_guessing', value: 8) }
     subject(:game_guessing) { described_class.new(0, 9, number_guessing) }
 
     # Before you write the #make_guess method:
@@ -123,7 +123,10 @@ describe FindNumber do
     # It will fail with an undefined method error because you haven't
     # written #make_guess yet!
     context 'when min is 0 and max is 9' do
-      xit 'returns 4' do
+      it 'returns 4' do
+        expectation = (0 + 9) / 2
+        guess = game_guessing.make_guess
+        expect(guess).to eq(expectation)
       end
     end
 
@@ -136,22 +139,34 @@ describe FindNumber do
     # random number double created inside this method's describe block.
 
     context 'when min is 5 and max is 9' do
-      xit 'returns 7' do
+      subject(:game_guessing) { described_class.new(5, 9, number_guessing) }
+      it 'returns 7' do
+        guess = game_guessing.make_guess
+        expect(guess).to eq(7)
       end
     end
 
     context 'when min is 8 and max is 9' do
+      subject(:game_guessing) { described_class.new(8, 9, number_guessing) }
       xit 'returns 8' do
+        guess = game_guessing.make_guess
+        expect(guess).to eq(8)
       end
     end
 
     context 'when min is 0 and max is 3' do
-      xit 'returns 1' do
+      subject(:game_guessing) { described_class.new(0, 3, number_guessing) }
+      it 'returns 1' do
+        guess = game_guessing.make_guess
+        expect(guess).to eq(1)
       end
     end
 
     context 'when min and max both equal 3' do
-      xit 'returns 3' do
+      subject(:game_guessing) { described_class.new(3, 3, number_guessing) }
+      it 'returns 3' do
+        guess = game_guessing.make_guess
+        expect(guess).to eq(3)
       end
     end
   end
